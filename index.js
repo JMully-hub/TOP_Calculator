3cd4aa2cb1a1043a2e583cd534fd1955695dc65a
JMully-hub <mulligan321@hotmail.com>
2022-11-24 23:11:41 +0000

    Added basic html and simple JS to start displaying the buttons and what has been pressed. This is a starting point to better understand the project.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function operate(a, operator, b){
+    switch (operator){
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+    }
+};
+
+function recordButton(event){
+    document.getElementById('calculation').innerText += event.target.value;
+}
+
 
 // init
-window.onload = makeGrid;
+window.onload = document.getElementById('calcInputForm').addEventListener('mouseup', (event) => {
+    recordButton(event);
+});
