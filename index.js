dfbe1dcbf6efcb925c60075e501ea3847a6ea7be
JMully-hub <mulligan321@hotmail.com>
2022-11-29 22:21:03 +0000

    Working on basic calc JS functions, created an object to store variables and operators for easier use. Some basic functionality with calculator but bug for pressing an operator and showing calculation needs fixing, where the total is not displayed correctly, concat instead of new

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function operate(a, operator, b){
+function operate(aStr, operator, bStr){
+    let a = parseFloat(aStr);
+    let b = parseFloat(bStr);
     switch (operator){
         case '+': return a + b;
         case '-': return a - b;
@@ -8,11 +10,55 @@ function operate(a, operator, b){
 };
 
 function recordButton(event){
-    document.getElementById('calculation').innerText += event.target.value;
+
+    if (event.target.value === 'Clear'){
+        document.getElementById('calculation').innerText = '';
+        runningCalculation.a = null;
+        runningCalculation.b = null;
+        runningCalculation.operator = null;
+        runningCalculation.memory = '';
+        return;
+    }
+
+
+    if (['+','-','*','/','='].includes(event.target.value)){
+        
+
+        if (!runningCalculation.a ){
+            runningCalculation.a = runningCalculation.memory;
+            document.getElementById('calculation').innerText = '';
+        }else{
+            runningCalculation.b = runningCalculation.memory;
+            document.getElementById('calculation').innerText = '';
+        }  
+
+        if (runningCalculation.a && runningCalculation.b && runningCalculation.operator){
+            runningCalculation.a = operate(runningCalculation.a, runningCalculation.operator, runningCalculation.b)
+            runningCalculation.operator = event.target.value;
+            runningCalculation.b = null;
+            runningCalculation.memory = '';
+            //document.getElementById('calculation').innerText = runningCalculation.a ;
+           
+        }
+        if (event.target.value !== '=') {
+            runningCalculation.operator = event.target.value;
+        }
+        runningCalculation.memory = '';
+    }else{
+        runningCalculation.memory += event.target.value;
+        document.getElementById('calculation').innerText += event.target.value;
+        }
+    
+  
+   
 }
 
 
 // init
+const runningCalculation = {a:null, b:null, operator:null, memory:''};
 window.onload = document.getElementById('calcInputForm').addEventListener('mouseup', (event) => {
-    recordButton(event);
+    if (event.target.type === 'button'){
+        recordButton(event);
+        console.log(runningCalculation);
+    }
 });
